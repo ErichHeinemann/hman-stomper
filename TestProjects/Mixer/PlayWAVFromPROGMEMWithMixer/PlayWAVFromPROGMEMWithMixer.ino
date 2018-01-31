f4068ea425e520f6e8dbd220aa0a3ca6558f1b7c
Erich <35889973+ErichHeinemann@users.noreply.github.com>
2018-01-31 18:02:10 +0100

    New Files for the new INO-Version.
    In this version the MIXER works basically. The current problem is only that the 2 sounds, - one with a short duration and one with a long duration starts always at the same time. Development is still in progress.

--- TestProjects/Mixer/PlayWAVFromPROGMEMWithMixer/PlayWAVFromPROGMEMWithMixer.ino
+++ TestProjects/Mixer/PlayWAVFromPROGMEMWithMixer/PlayWAVFromPROGMEMWithMixer.ino
@@ -9,24 +9,18 @@
 #include "AudioFileSourcePROGMEM.h"
 #include "AudioGeneratorWAV.h"
 #include "AudioOutputI2SDAC.h"
-
-
-#include "AudioOutputBuffer.h"
-
 #include "AudioMixerOutBuffer.h"
 #include "AudioMixerInBuffer.h"
 
 
-// VIOLA sample taken from https://ccrma.stanford.edu/~jos/pasp/Sound_Examples.html
-#include "viola.h"
-
+// Sample, 16 Bit, Mono, Midi c4 - Note 60
+#include "angels.h"
+#include "arabicbass.h"
 AudioGeneratorWAV *wav1;
 AudioGeneratorWAV *wav2;
 AudioFileSourcePROGMEM *file1;
 AudioFileSourcePROGMEM *file2;
 AudioOutputI2SDAC *out;
-
-AudioOutputBuffer *buf1;
 
 AudioMixerInBuffer *channel1;
 AudioMixerInBuffer *channel2;
@@ -38,62 +32,74 @@ void setup()
 
   Serial.begin(115200);
   delay(1000);
-  file1 = new AudioFileSourcePROGMEM( viola, sizeof(viola) );
-  // file2 = new AudioFileSourcePROGMEM( viola, sizeof(viola) );
+  file1 = new AudioFileSourcePROGMEM( angels, sizeof( angels ) ); // long duration
+  // file1 = new AudioFileSourcePROGMEM( arabicbass, sizeof( arabicbass ) ); // sound with short duration
+  file2 = new AudioFileSourcePROGMEM( arabicbass, sizeof( arabicbass ) ); // sound with short duration
+
   out = new AudioOutputI2SDAC();
-  buf1 = new AudioOutputBuffer(8, out );
-  //Adjust Volume of Output
+  // Adjust Volume of Output
   out->SetGain(0.2);
   
   wav1 = new AudioGeneratorWAV();
-
+  wav2 = new AudioGeneratorWAV();
+  
   Serial.println("Define MainOut of Mixer");
   // new with Mixer, connect mainOut to the output
-  mainOut = new AudioMixerOutBuffer( 8, out );
+  mainOut = new AudioMixerOutBuffer( 2, out );
   mainOut->SetRate(44100);
-  mainOut->SetChannels(1); // 2=Stereo, 1=Mono
+  mainOut->SetChannels(2); // 2=Stereo, 1=Mono
   mainOut->SetBitsPerSample(16); // 16 Bit
 
-
-
-  Serial.println("Define Channel1 of Mixer");
-  // define channel1 of the Mixer and strip it to mainOut as Strip 1
-  channel1 = new AudioMixerInBuffer( 8, mainOut, 1 );
+  Serial.println("Begin MainOut");
   mainOut->begin();
 
-  
-  // channel1->begin();
-  Serial.println("Define Channel2 of Mixer");  
+  // define channel1 of the Mixer and strip it to mainOut as Strip 1
+  Serial.println("Define Channel1 of Mixer");
+  channel1 = new AudioMixerInBuffer( 2, mainOut, 1 );
+
   // define channel2 of the Mixer and strip it to mainOut as Strip 2
-  channel2 = new AudioMixerInBuffer( 64, mainOut, 2 );
-  wav2 = new AudioGeneratorWAV();
-  Serial.println("Begin Wav1");
-  wav1->begin(file1, channel1); // channel 1
-  delay(100);
-
-Serial.println("Begin Wav2");
+  Serial.println("Define Channel2 of Mixer");  
+  channel2 = new AudioMixerInBuffer( 2, mainOut, 2 );
+  Serial.println("Begin Wav2");
   channel2->begin();
+
+
+  Serial.println("Begin Wav1");
+  wav1->begin(file1, channel1); // channel1
+    delay(200);
   wav2->begin(file2, channel2);
-Serial.println("Begin MainOut");
-
-
  
 }
 
 void loop()
 {
-  if (wav1->isRunning()) {
-    if (!wav1->loop()) wav1->stop();
+  if (wav1->isRunning() ) {
+    if (!wav1->loop() ) {
+      
+      wav1->stop(); 
+      channel1->stop();
+      }
   } else {
     Serial.printf("WAV1 done\n");
-    delay(1000);
+    wav1->begin(file1, channel1);
+    Serial.printf("restart wav1\n");
+    file1 = new AudioFileSourcePROGMEM( angels, sizeof( angels ) ); // long duration
+    wav1->begin(file1, channel1); // channel1
+    
   }
 
-  if (wav2->isRunning()) {
-    if (!wav2->loop()) wav2->stop();
+  if (wav2->isRunning() ) {
+    if (!wav2->loop() ) {
+      wav2->stop(); 
+      channel2->stop();
+    }
   } else {
     Serial.printf("WAV2 done\n");
-    delay(1000);
+
+    Serial.printf("restart wav2\n");
+    file2 = new AudioFileSourcePROGMEM( arabicbass, sizeof( arabicbass ) ); // sound with short duration
+    // channel2 = new AudioMixerInBuffer( 2, mainOut, 4);
+    wav2->begin(file2, channel2);
   }
 
 }
